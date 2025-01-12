bd7373259b7efae6b3143a3b43ad799cce33e257
Sotiris Morfakidis <sotirismorfakidis@gmail.com>
2025-01-12 19:46:53 +0200

    add latex treesitter

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -13,7 +13,7 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   pattern = { 'markdown', 'tex', 'text' },
   callback = function()
     vim.opt_local.spell = true
-    vim.opt_local.spelllang = 'el'
+    vim.opt_local.spelllang = 'el,en'
     vim.opt_local.textwidth = 80
   end,
 })
